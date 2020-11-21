e00362e270562bf93acbb2c591772934309be355
kevinazo83 <73851274+kevinazo83@users.noreply.github.com>
2020-11-21 14:57:19 -0500

    Update menu.py

--- Turret-madness/menu.py
+++ Turret-madness/menu.py
@@ -17,9 +17,9 @@ boton=p.image.load("images\\boton.png")
 back=p.image.load("images\\flecha-regresar.png")
 select=p.mixer.Sound('efectos\\sonido_boton.wav')
 tap=p.mixer.Sound('efectos\\boton_tap.wav')
-p.mixer.music.load("effectos\\fondo.wav")
-p.mixer.music.play(-1)
-p.mixer.music.set_volume(0.2)
+#p.mixer.music.load("effectos\\fondo.wav")
+#p.mixer.music.play(-1)
+#p.mixer.music.set_volume(0.2)
 canal1=p.mixer.Channel(0)
 canal2=p.mixer.Channel(1)
 
@@ -38,15 +38,17 @@ def main_menu():
     pos=540,300
     k,k1,k2=0,0,0
     while running:
+        casilla=False
         pantalla.blit(bg_image,(0,0))
         draw_txt("TURRET",font_2, (0,0,0),pantalla,335,65)
         draw_txt("TURRET",font_2, (255,255,255),pantalla,332,50)
         draw_txt("MADNESS",font_1, (0,0,0),pantalla,381,185)
         draw_txt("MADNESS",font_1, (255,255,255),pantalla,368,170)
-        if not (boton_1==None or boton_2==None or boton_3==None):
+        if not(boton_1==None or boton_2==None or boton_3==None): 
             pantalla.blit(bg_image,boton_1,boton_1)
             pantalla.blit(bg_image,boton_2,boton_2)       
-            pantalla.blit(bg_image,boton_3,boton_3)  
+            pantalla.blit(bg_image,boton_3,boton_3)
+             
         mx,my=p.mouse.get_pos()
         boton_1=pantalla.blit(boton,pos)
         boton_2=pantalla.blit(boton,(pos[0],pos[1]+100))
@@ -59,6 +61,7 @@ def main_menu():
         draw_txt("CONTROLES",font_3, (255,255,255),pantalla,552,508)
         
         if boton_1.collidepoint((mx,my)):
+            casilla=True
             pantalla.blit(bg_image,boton_1,boton_1)
             boton_1=pantalla.blit(boton,(pos[0],pos[1]-5))
             draw_txt("JUGAR",font, (0,0,0),pantalla,580,303)
@@ -68,11 +71,13 @@ def main_menu():
                 k+=1
             if click:
                 canal2.play(select)
-                Niveles.niveles()
                 click=False
+                Niveles.niveles()
         else:
             k=0
+            
         if boton_2.collidepoint((mx,my)):
+            casilla=True
             pantalla.blit(bg_image,boton_2,boton_2)
             boton_2=pantalla.blit(boton,(pos[0],pos[1]+95))
             draw_txt("OPCIONES",font, (0,0,0),pantalla,554,403)
@@ -85,7 +90,9 @@ def main_menu():
                 opciones()
         else:
             k1=0
+            
         if boton_3.collidepoint((mx,my)):
+            casilla=True
             pantalla.blit(bg_image,boton_3,boton_3)
             boton_3=pantalla.blit(boton,(pos[0],pos[1]+195))
             draw_txt("CONTROLES",font_3, (0,0,0),pantalla,554,506)
@@ -97,6 +104,7 @@ def main_menu():
                 canal1.play(select)
                 opciones()
         else:
+            
             k2=0
         for event in p.event.get():
             if event.type==p.QUIT:
@@ -106,7 +114,8 @@ def main_menu():
                 if event.key==p.K_ESCAPE:
                     running==False
             elif event.type==p.MOUSEBUTTONDOWN:
-                if (event.button==1 and boton_1.collidepoint((mx,my))) or (event.button==1 and boton_2.collidepoint((mx,my))) or (event.button==1 and boton_3.collidepoint((mx,my))):
+                
+                if event.button==1 and casilla:
                     click=True
         p.display.update()
         clock.tick(60)
@@ -114,12 +123,14 @@ def opciones():
     running = True 
     click=False
     while running:
+        casilla=False
         pantalla.blit(bg_image,(0,0))
         draw_txt("opciones",font_1, (0,0,0),pantalla,343,83)
         draw_txt("opciones",font_1, (255,255,255),pantalla,330,80)
         mx,my=p.mouse.get_pos()
         boton_b=pantalla.blit(back,(50,630))
         if boton_b.collidepoint((mx,my)):
+            casilla=True
             pantalla.blit(bg_image,boton_b,boton_b)
             boton_b=pantalla.blit(back,(50,625))
             canal1.play(tap)
@@ -135,21 +146,24 @@ def opciones():
                     running==False
                     main_menu()
             elif event.type==p.MOUSEBUTTONDOWN:
-                if event.button==1 and boton_b.collidepoint((mx,my)):
+                if event.button==1 and casilla:
                     click=True
         p.display.update()
         clock.tick(60)
 
 def controles():
+    
     running = True 
     click=False
     while running:
+        casilla=False
         pantalla.blit(bg_image,(0,0))
         draw_txt("CONTROLES",font_1, (0,0,0),pantalla,343,83)
         draw_txt("CONTROLES",font_1, (255,255,255),pantalla,330,80)
         mx,my=p.mouse.get_pos()
         boton_b=pantalla.blit(back,(50,630))
         if boton_b.collidepoint((mx,my)):
+            casilla=True
             pantalla.blit(bg_image,boton_b,boton_b)
             boton_b=pantalla.blit(back,(50,625))
             canal1.play(tap)
@@ -165,8 +179,9 @@ def controles():
                     running==False
                     main_menu()
             elif event.type==p.MOUSEBUTTONDOWN:
-                if event.button==1 and boton_b.collidepoint((mx,my)):
+                if event.button==1 and casilla:
                     click=True
         p.display.update()
         clock.tick(60)
 main_menu()
+
